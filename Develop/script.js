e672b5f70f881515d70abd8fb8d85f7e4d52b250
Mark McDonald <markwilliammcdonald@gmail.com>
2020-05-09 20:48:50 -0500

    added the confirms to survey the user on what to include and what to exclude in ther password

--- Develop/script.js
+++ Develop/script.js
@@ -12,15 +12,33 @@ let generatedPassword = [];
 //this is from the base code from class and will target the button
 let generateBtn = document.querySelector("#generate");
 
+let passwordText = document.querySelector("#password");
+
 // Write password to the #password input
 function writePassword() {
   let pwLength = prompt("How many characters would you like your password to be? (Please select a number between 8 and 128)");
-  let useLowercase = confirm("Do you want to include lowercase characters? Cancel for 'no' and Okay for 'yes'");
-  let password = generatePassword();
-  let passwordText = document.querySelector("#password");
+// Get whatever the user inputs and turn it into an integer 
+  pwLength = parseInt(pwLength);
+  //define if it is in range of 8 - 128 chars
+  if (pwLength >= 8 && pwLength <= 128) {
+    passwordArray = [];
+    generatedPassword =[];
 
+  let useLowercase = confirm("Do you want to include lowercase characters? Cancel for 'no' and Okay for 'yes'");
+  let useUppercase = confirm("Do you want to include uppercase characters? Cancel for 'no' and Okay for 'yes'");
+  let useNumbers = confirm("Do you want to include numbers? Cancel for 'no' and Okay for 'yes'");
+  let useSpecial = confirm("Do you want to include special characters? Cancel for 'no' and Okay for 'yes'");
+
+  //then, need to check true or false for these confrims above
+
+
+  let password = generatePassword(); //not sure if I need this ??
+
+
+
+
+  }
 //These are my throught on what I might need for logic and functions
-  // if (pwLength >= 8 && ....) define if it is range 
 
     //  if lowercase == true
         //input come certain number of lowercase characters
@@ -56,6 +74,7 @@ function hasSpecialCharacters (password){}
 function  (password){}
 
 
+**** ALSO NEED TO INPUT SOMETHING THAT WILL WARN USER WHEN A CRTITERA IS NOT MET*****
 */
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
